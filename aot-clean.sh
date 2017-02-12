ef7cdcea9d8d0e361eb6bf440755a69f159ad193
lunargorge <lunargorge@gmail.com>
2017-02-12 15:19:30 +0100

    fix aot-clean.sh

--- aot-clean.sh
+++ aot-clean.sh
@@ -6,5 +6,5 @@ find ./src -name "*.ngsummary.json" -exec rm -rf {} \;
 find ./src -name "*.shim.ngstyle.ts" -exec rm -rf {} \;
 find ./src -name "*.css.ngstyle.ts" -exec rm -rf {} \;
 find ./src -name "*.js.map" -exec rm -rf {} \;
-find ./src -name "*.d.ts" -exec rm -rf {} \;
+find ./src/app -name "*.d.ts" -exec rm -rf {} \;
 find ./src -name "*.metadata.json" -exec rm -rf {} \;
